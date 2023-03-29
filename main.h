d7f5356390afbbd325855b7b61cbb476436699e3
ndukasamuel <chukdyyoung929@gmail.com>
2023-03-28 20:55:49 -0700

    first

--- main.h
+++ main.h
@@ -1,3 +1,4 @@
+
 #ifndef MAIN_H
 #define MAIN_H
 #include <stdarg.h>
@@ -114,4 +115,5 @@ int is_digit(char);
 long int convert_size_number(long int num, int size);
 long int convert_size_unsgnd(unsigned long int num, int size);
 
-#endif
+#endif /* MAIN_H */
+
